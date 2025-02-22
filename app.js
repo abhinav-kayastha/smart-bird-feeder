ee3c59e15d8242ed6a387025482d1806630be97d
abhinav-kayastha <Abhinav.Kayastha@metropolia.fi>
2025-02-22 12:20:40 +0200

    implementing mqtt to web server from previous course, changes still need to be done

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const mqtt = require("mqtt");
 const path = require("path");
+const util = require("util");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 const passport = require("./config/passport");
 const session = require("express-session");
 const db = require("./config/database");
-const readline = require("readline");
 const app = express();
 
-// Middleware for parsing request bodies
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+// Create variables for MQTT use here
+const address = "mqtt://18.198.188.151:21883";
+const topic = "abhinav/LED";
+
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Initialize session middleware
@@ -22,6 +32,49 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+function read(filePath = "./message.json") {
+  return readFile(path.resolve(__dirname, filePath)).then((data) =>
+    JSON.parse(data)
+  );
+}
+
+function write(data, filePath = "./message.json") {
+  return writeFile(path.resolve(__dirname, filePath), JSON.stringify(data));
+}
+
+// create an MQTT instance
+const client = mqtt.connect(address, {
+  clientId: "abhinav",
+  username: "SmartIoT",
+  password: "SmartIoTMQTT",
+}); // connecting to mqtt broker, using the clientId: abhinav
+
+// Check that you are connected to MQTT and subscribe to a topic (connect event)
+client.on("connect", () => {
+  //when connect
+  client.subscribe(topic); // then subscribes to topic abhinav
+  console.log("connected to MQTT"); // prints that it connects to MQTT
+});
+
+// handle instance where MQTT will not connect (error event)
+client.on("error", (error) => {
+  console.log("Error: ", error);
+});
+
+// Handle when a subscribed message comes in (message event)
+client.on("message", (topic, message) => {
+  // event will trigger when client gets a msg from
+  // the topic they sub to
+  console.log(message.toString());
+  // when msg is received, it is added to the message.json file
+  // read, then push, then write
+  read().then((messages) => {
+    messages.push({ id: new Date().getTime().toString(), msg: message.toString() });
+    write(messages);
+  });
+});
+
+// Route to serve the home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
 });
@@ -56,37 +109,45 @@ function ensureAuthenticated(req, res, next) {
   res.redirect("/");
 }
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-  promptUserInput(); // Start prompting user input after the server starts
+// Route to serve the JSON array from the file message.json when requested from the home page
+app.get("/messages", async (req, res) => {
+  const messages = await read();
+  res.json(messages);
 });
 
-// Function to prompt user input from the console
-function promptUserInput() {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+// Route to serve the page to add a message
+app.get("/add", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./message.html"));
+});
 
-  rl.question("Enter status: ", (status) => {
-    const datetime = new Date().toLocaleString();
-    insertStatus(datetime, status);
-    rl.close();
-    promptUserInput(); // Prompt again for continuous input
-  });
-}
+// Route to show a selected message. Note, it will only show the message as text. No html needed
+app.get("/:id", async (req, res) => {
+  const messages = await read(); // reads the current messages
+  const message = messages.find((m) => m.id === req.params.id);
+  res.send(message);
+});
 
-// Function to insert status and datetime into the database
-function insertStatus(datetime, status) {
-  db.run(
-    "INSERT INTO activity (time, status) VALUES (?, ?)",
-    [datetime, status],
-    (err) => {
-      if (err) {
-        console.error("Error inserting data:", err.message);
-      } else {
-        console.log("Data inserted successfully");
-      }
-    }
-  );
-}
+// Route to CREATE a new message on the server and publish to mqtt broker
+app.post("/", async (req, res) => {
+  const newMessage = { id: new Date().getTime().toString(), msg: req.body.msg };
+  const messages = await read();
+  messages.push(newMessage);
+  await write(messages);
+  client.publish(req.body.topic || topic, newMessage.msg);
+  res.sendStatus(200);
+});
+
+// Route to delete a message by id
+app.delete("/:id", async (req, res) => {
+  try {
+    const messages = await read();
+    await write(messages.filter((c) => c.id !== req.params.id));
+    res.sendStatus(200);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
